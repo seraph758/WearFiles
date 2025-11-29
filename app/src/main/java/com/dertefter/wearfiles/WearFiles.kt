1682442acaaa9aacee4a2c017e653f4b6726ace9
Серебряков Сергей Юрьевич <dertefter@gmail.com>
2025-11-29 22:10:29 +0700

    started from scratch

--- app/src/main/java/com/dertefter/wearfiles/WearFiles.kt
+++ app/src/main/java/com/dertefter/wearfiles/WearFiles.kt
@@ -1,11 +1,5 @@
 package com.dertefter.wearfiles
 import android.app.Application
-import android.content.Context
-import android.os.Environment
-import android.util.Log
 
 
-
-class WearFiles : Application() {
-
-}
+class WearFiles : Application()
--- app/src/main/java/com/dertefter/wearfiles/model/Action.kt
+++
@@ -1,6 +0,0 @@
-package com.dertefter.wearfiles.model
-
-data class Action(
-    val name: Int,
-    val type: ActionType,
-)

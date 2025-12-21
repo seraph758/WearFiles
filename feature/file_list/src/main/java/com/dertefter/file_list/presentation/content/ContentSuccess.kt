c666d6c2f9c0dbf1e4ba1424422b405a984471c7
Серебряков Сергей Юрьевич <dertefter@gmail.com>
2025-12-21 20:26:16 +0700

    remove unused icon imports and update FileItem parameters

--- feature/file_list/src/main/java/com/dertefter/file_list/presentation/content/ContentSuccess.kt
+++ feature/file_list/src/main/java/com/dertefter/file_list/presentation/content/ContentSuccess.kt
@@ -2,10 +2,6 @@ package com.dertefter.file_list.presentation.content
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.InsertDriveFile
-import androidx.compose.material.icons.filled.Folder
-import androidx.compose.material.icons.filled.Watch
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -18,6 +14,7 @@ import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
 import androidx.wear.compose.foundation.lazy.TransformingLazyColumn
 import androidx.wear.compose.foundation.lazy.rememberTransformingLazyColumnState
 import androidx.wear.compose.material3.ScreenScaffold
@@ -26,6 +23,7 @@ import com.dertefter.data.model.PrettyPath
 import com.dertefter.design.components.items.BottomBarItem
 import com.dertefter.design.components.items.FileItem
 import com.dertefter.design.components.items.PathItem
+import com.dertefter.design.icons.Icons
 import com.dertefter.file_list.presentation.Action
 import com.dertefter.file_list.presentation.Event
 import com.dertefter.file_list.utils.isImage
@@ -49,7 +47,7 @@ fun ContentSuccess(
     ScreenScaffold(
         scrollState = columnState,
         contentPadding = PaddingValues(
-            top = 52.dp, start = 10.dp, end = 10.dp, bottom = 10.dp
+            top = 52.dp, start = 10.dp, end = 10.dp, bottom = 48.dp
         ),
     ) { contentPadding ->
 
@@ -69,34 +67,18 @@ fun ContentSuccess(
                     transformationSpec = transformationSpec,
                     fullPath = fullPath,
                     homePath = homePath,
-                    homeIcon = Icons.Default.Watch,
+                    homeIcon = Icons.Watch,
                 )
             }
 
             for (file in files) {
 
                 item {
-
-                    var thumbnail by remember { mutableStateOf<ImageBitmap?>(null) }
-                    val scope = rememberCoroutineScope()
-
-                    if (file.isImage()) {
-                        LaunchedEffect(file) {
-                            scope.launch {
-                                file.loadThumbnail()?.let {
-                                    thumbnail = it.asImageBitmap()
-                                }
-                            }
-                        }
-                    }
-
-                    val icon =
-                        if (file.isDirectory) Icons.Filled.Folder else Icons.AutoMirrored.Filled.InsertDriveFile
                     FileItem(
                         transformationSpec,
                         text = file.name,
-                        icon = icon,
-                        thumbnail = thumbnail,
+                        thumbnailUrl = file.absolutePath,
+                        file = file,
                         onClick = {
                             if (file.isFile) {
                                 onEvent(Event.OnFileClick(file))

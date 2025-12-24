3a44bab3942d65bd83f70c89a23e71555e0dae7d
Серебряков Сергей Юрьевич <dertefter@gmail.com>
2025-12-24 20:14:34 +0700

    bump version to 2.0.1 and add new banner screenshot

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.dertefter.wearfiles"
         minSdk = 26
         targetSdk = 36
-        versionCode = 30
-        versionName = "2.0.0"
+        versionCode = 31
+        versionName = "2.0.1"
     }
 
     buildTypes {
